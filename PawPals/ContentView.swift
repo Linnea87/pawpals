836bb4c50366e92b3a6f5cf48876db69e21b8411
Linnéa <linnea.ternevik@gmail.com>
2026-05-13 12:36:15 +0200

    initial commit with .gitignore file

---
+++ PawPals/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PawPals
+//
+//  Created by Linnéa on 2026-05-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PawPals/PawPalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawPalsApp.swift
+//  PawPals
+//
+//  Created by Linnéa on 2026-05-13.
+//
+
+import SwiftUI
+
+@main
+struct PawPalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
