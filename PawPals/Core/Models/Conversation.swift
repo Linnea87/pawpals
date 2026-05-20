6c322f40fdd32a7816c59016303126f286455869
PatNoO <Noordh94@hotmail.se>
2026-05-20 20:22:34 +0200

    style [PP-038] Redesign conversation row and add unreadCount to model

--- PawPals/Core/Models/Conversation.swift
+++ PawPals/Core/Models/Conversation.swift
@@ -5,4 +5,5 @@ struct Conversation: Identifiable, Codable {
     var participantIDs: [String]
     var lastMessage: String
     var lastMessageTimestamp: Date
+    var unreadCount: Int = 0 // // ← default 0 so existing code doesn't break code coming in PP-023 
 }
