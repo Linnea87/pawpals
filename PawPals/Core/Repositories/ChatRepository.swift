ae4d112d24694888c88570efe3cfb7c737b53fb6
PatNoO <Noordh94@hotmail.se>
2026-05-20 09:39:54 +0200

    feat [PP-022] Add sendMessage and observeMessages to ChatRepository and ChatService

--- PawPals/Core/Repositories/ChatRepository.swift
+++ PawPals/Core/Repositories/ChatRepository.swift
@@ -2,5 +2,12 @@ import Foundation
 
 protocol ChatRepository {
     func fetchConversations(for userId: String) async throws -> [Conversation]
+
+    func sendMessage(_ message: Message, to conversationID: String) async throws
+
+    func observeMessages(
+        conversationID: String,
+        onUpdate: @escaping ([Message]) -> Void
+    ) -> (() -> Void)
 }
 
