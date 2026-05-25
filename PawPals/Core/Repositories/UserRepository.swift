cb7df06fc33e91beba33c76b42da9863868a369d
Ahmad <ahmadalhayel20@gmail.com>
2026-05-25 15:30:36 +0200

    feat [PP-008] Add savePreferences and loadPreferences to UserRepository protocol

--- PawPals/Core/Repositories/UserRepository.swift
+++ PawPals/Core/Repositories/UserRepository.swift
@@ -6,6 +6,8 @@ protocol UserRepository {
     func updateProfile(_ user: User) async throws
     func saveDog(_ dog: Dog, userId: String) async throws
     func updateLocation(_ location: GeoPoint, userId: String) async throws
+    func savePreferences(_ prefs: UserPreferences, userId: String) async throws
+    func loadPreferences(userId: String) async throws -> UserPreferences
 
     
 }
