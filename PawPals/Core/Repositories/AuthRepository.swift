9acdea2d45a93b2ad3ae3da20804c7b6fa5d4842
Linnéa <linnea.ternevik@gmail.com>
2026-05-27 16:18:59 +0200

    feat [PP-002] Implement Sign Up with Google

--- PawPals/Core/Repositories/AuthRepository.swift
+++ PawPals/Core/Repositories/AuthRepository.swift
@@ -2,7 +2,7 @@ import Foundation
 
 protocol AuthRepository {
     func signUp(email: String, password: String) async throws -> User
-    func signUpWithGoogle() async throws
+    func signUpWithGoogle() async throws -> User
     func signOut() throws
     func signIn(email: String, password: String) async throws -> User
     func signInWithGoogle() async throws -> User
