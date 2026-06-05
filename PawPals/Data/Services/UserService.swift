682f62f8823ee1b3ee148bc3f054e5a16ca126f3
ruthpaulsson <ruth.paulsson@gmail.com>
2026-06-05 10:05:18 +0200

    refactor Replace doc.data(as:) with fetchUser in UserService

--- PawPals/Data/Services/UserService.swift
+++ PawPals/Data/Services/UserService.swift
@@ -155,7 +155,7 @@ final class UserService: UserRepository {
         var users: [User] = []
         for id in savedIds {
             let doc = try await db.collection("users").document(id).getDocument()
-            if let user = try? doc.data(as: User.self) {
+            if let user = try? await fetchUser(userId: id) {
                 users.append(user)
             }
         }
